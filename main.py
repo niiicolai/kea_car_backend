982f71add538f2193729f92da5fe56de28a01c9e
OliverRoat <oliver.roat@gmail.com>
2024-09-18 12:46:56 +0200

    removed more frontend

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 from db import test_db_connection
 from api import get_data_from_api
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-# Route for the home page
-@app.route('/')
-def home():
-    return "<h1>Welcome to the Car Sales Shop</h1>"
-
-# Add more routes as needed for different parts of the web application
-@app.route('/inventory')
-def inventory():
-    return "<h1>Inventory Page</h1>"
 
 if __name__ == '__main__':
     print("Testing database connection...")
@@ -23,4 +10,3 @@ if __name__ == '__main__':
     data = get_data_from_api(url)
     if data:
         print(f"API data: {data}")
-        app.run(debug=True)
