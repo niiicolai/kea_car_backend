0a720c6772df21819236a750e02814be6aba4c82
Marcus <marc303i@stud.kea.dk>
2024-11-12 17:54:40 +0100

    finished making a script for inserting data in to the mongodb

--- scripts/seed_mongodb.py
+++ scripts/seed_mongodb.py
@@ -1,20 +1,42 @@
 import json
 import os
 from dotenv import load_dotenv
-from pymongo import MongoClient
-from pymongo.database import Database
+from pymongo import MongoClient, UpdateOne
+
 
 load_dotenv()
 
-MONGO_DB_HOST=os.getenv("MONGO_DB_HOST")
-MONGO_DB_PORT=os.getenv("MONGO_DB_PORT")
-MONGO_DB_NAME=os.getenv("MONGO_DB_NAME")
+MONGO_DB_HOST = os.getenv("MONGO_DB_HOST")
+MONGO_DB_PORT = os.getenv("MONGO_DB_PORT")
+MONGO_DB_NAME = os.getenv("MONGO_DB_NAME")
 
-accessories_file_path = "mongodb_insert_data.json"
+collections = [
+    'accessories',
+    'insurances',
+    'customers',
+    'sales_people',
+    'colors',
+    'brands',
+    'models'
+]
 
-def read_json(file_path):
-    with open(file_path, 'r') as file:
+
+def read_json():
+    with open("mongodb_insert_data.json", 'r') as file:
         data = json.load(file)
     return data
 
 
+if __name__ == '__main__':
+    data = read_json()
+    client = MongoClient(host=MONGO_DB_HOST, port=int(MONGO_DB_PORT))
+    db = client[MONGO_DB_NAME]
+
+    for collection_name in collections:
+        bulk_operations = [
+            UpdateOne({'_id': doc['_id']}, {'$set': doc}, upsert=True)
+            for doc in data[collection_name]
+        ]
+        db.get_collection(collection_name).bulk_write(bulk_operations)
+
+    client.close()
--- scripts/start_and_restore.sh
+++ scripts/start_and_restore.sh
@@ -6,5 +6,8 @@ bash ./scripts/wait-for-it.sh mysql:3306 --timeout=30 --strict -- echo "MySQL is
 echo "Restoring MySQL dump..."
 python ./scripts/restore_mysql.py --filepath="./scripts/mysql.sql"
 
+echo "Restoring MongoDB dump..."
+python ./scripts/seed_mongodb.py
+
 echo "Starting FastAPI server..."
 uvicorn main:app --host 0.0.0.0 --port 8000
