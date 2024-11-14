9b5da39e5b678c9ef944ab8cc02d9c9dbcca96d9
OliverRoat <oliver.roat@gmail.com>
2024-11-14 12:20:43 +0100

    fix

--- app/controllers/error_handler.py
+++ app/controllers/error_handler.py
@@ -1,8 +1,6 @@
 # External Library imports
 from typing import Callable
 import logging
-import os
-from dotenv import load_dotenv
 
 from fastapi import HTTPException, status
 
@@ -84,11 +82,10 @@ def error_handler(error_message: str, callback: Callable):
 
     except Exception as e:
         log_error(error_message, e)
-        load_dotenv()
         # Raise a generic internal server error for the client
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=str(f"Internal Server Error Caught: {error_message}. {e}. {os.getenv('SECRET_KEY')}")
+            detail=str(f"Internal Server Error Caught: {error_message})")
         )
 
 def log_error(error_message: str, error: Exception):
