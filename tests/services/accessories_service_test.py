70e586c99546f8a97809f75c690ad5240309d3d2
Marcus <marc303i@stud.kea.dk>
2024-11-27 20:52:17 +0100

    Put back what Daniel removed

---
+++ tests/services/accessories_service_test.py
@@ -0,0 +1,17 @@
+import pytest
+from app.services import accessories_service
+from app.exceptions.database_errors import UnableToFindIdError
+from app.resources.accessory_resource import AccessoryReturnResource
+
+# VALID TESTS FOR get_accessory_by_id
+
+
+# INVALID TESTS FOR get_accessory_by_id
+
+
+# VALID TESTS FOR get_all_accessories
+
+
+# INVALID TESTS FOR get_all_accessories
+
+
---
+++ tests/services/insurances_service_test.py
@@ -0,0 +1,12 @@
+import pytest
+from app.services import insurances_service
+from app.exceptions.database_errors import UnableToFindIdError
+from app.resources.insurance_resource import InsuranceReturnResource
+
+# VALID TESTS FOR get_insurance_by_id
+
+# INVALID TESTS FOR get_insurance_by_id
+
+# VALID TESTS FOR get_all_insurances
+
+# INVALID TESTS FOR get_all_insurances
