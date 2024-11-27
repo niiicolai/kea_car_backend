c20e6889de220770cf01978f95f23c21674b84fc
niiicolai <niiicolai@gmail.com>
2024-11-27 10:44:19 +0100

    move events to its own file

--- scripts/mysql.sql
+++ scripts/mysql.sql
@@ -562,18 +562,3 @@ VIEW `car_purchase_view` AS
         `cars` car
         LEFT JOIN `purchases` purchase ON purchase.cars_id = car.id;
 
---
--- Dumping events for database 'kea_cars_dev'
---
-DROP EVENT IF EXISTS delete_old_none_purchased_cars;
-DELIMITER //
-CREATE EVENT delete_old_none_purchased_cars
-ON SCHEDULE EVERY 1 DAY
-DO
-BEGIN
-	DELETE car
-	FROM cars car
-	INNER JOIN car_purchase_view cpv ON car.id = cpv.car_id
-	WHERE cpv.is_past_deadline = TRUE;
-END //
-DELIMITER ;
