8ef4af91e3840ab8f66a3c78cd6df06eab2bc42b
Marcus <marc303i@stud.kea.dk>
2024-12-09 18:54:53 +0100

    for some reason did our secret key env value not get loaded, which meant that a test that worked before was failing

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordBearer
+
+load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
