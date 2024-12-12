14c2909cf9afdfaa388bb0acd57ab236934fb057
DanielCausevic <noturnalldepression@gmail.com>
2024-12-12 14:49:00 +0100

    insurance service test virker nu

--- tests/services/insurances_service_test.py
+++ tests/services/insurances_service_test.py
@@ -43,6 +43,162 @@ def test_get_insurance_by_id_with_valid_partitions(
 
 # INVALID TESTS FOR get_insurance_by_id
 
+@pytest.mark.parametrize("invalid_insurance_id, expected_error, expected_error_message", [
+    (None, TypeError, "insurance_id must be of type str, not NoneType."),
+    (1, TypeError, "insurance_id must be of type str, not int."),
+    (True, TypeError, "insurance_id must be of type str, not bool."),
+    ("invalid_id", UnableToFindIdError, "Insurance with ID: invalid_id does not exist."),
+])
+def test_get_insurance_by_id_with_invalid_repository_id_partitions(
+        mySQLInsuranceRepository, invalid_insurance_id, expected_error, expected_error_message
+):
+    with pytest.raises(expected_error, match=expected_error_message):
+        insurances_service.get_by_id(
+            repository=mySQLInsuranceRepository,
+            insurance_id=invalid_insurance_id
+        )
+
+@pytest.mark.parametrize("invalid_repository, expected_error_message", [
+    (None, "repository must be of type InsuranceRepository, not NoneType."),
+    ("repository", "repository must be of type InsuranceRepository, not str."),
+    (True, "repository must be of type InsuranceRepository, not bool."),
+    (123, "repository must be of type InsuranceRepository, not int."),
+])
+def test_get_insurance_by_id_with_invalid_repository_partitions(
+        invalid_repository, expected_error_message
+):
+    valid_insurance_id = "37074fac-26da-4e38-9ae6-acbe755359e5"
+
+    with pytest.raises(TypeError, match=expected_error_message):
+        insurances_service.get_by_id(
+            repository=invalid_repository,
+            insurance_id=valid_insurance_id
+        )
+
+
+def test_get_insurance_by_id_with_invalid_repository_partition_types(
+        mySQLAccessoryRepository, mySQLCustomerRepository, mySQLBrandRepository
+):
+    valid_insurance_id = "37074fac-26da-4e38-9ae6-acbe755359e5"
+
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLAccessoryRepository).__name__}."):
+        insurances_service.get_by_id(
+            repository=mySQLAccessoryRepository,
+            insurance_id=valid_insurance_id
+        )
+
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLCustomerRepository).__name__}."):
+        insurances_service.get_by_id(
+            repository=mySQLCustomerRepository,
+            insurance_id=valid_insurance_id
+        )
+
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLBrandRepository).__name__}."):
+        insurances_service.get_by_id(
+            repository=mySQLBrandRepository,
+            insurance_id=valid_insurance_id
+        )
 # VALID TESTS FOR get_all_insurances
 
+def test_get_all_insurances_with_valid_partitions(mySQLInsuranceRepository):
+    insurances = insurances_service.get_all(repository=mySQLInsuranceRepository)
+
+    assert isinstance(insurances, list), \
+        (f"Expected instance of list, "
+         f"but got: {type(insurances).__name__}")
+
+    expected_amount_of_insurances = 5
+    actual_amount_of_insurances = len(insurances)
+
+    assert actual_amount_of_insurances == expected_amount_of_insurances, \
+        (f"Expected {expected_amount_of_insurances} insurances, "
+         f"but got: {actual_amount_of_insurances}")
+
+    for insurance in insurances:
+        assert isinstance(insurance, InsuranceReturnResource), \
+            (f"Expected instance of InsuranceReturnResource, "
+             f"but got: {type(insurance).__name__}")
+
+@pytest.mark.parametrize("valid_insurances_limit, expected_amount_of_insurances", [
+    (-1, 5),
+    (0, 5),
+    (1, 1),
+    (5, 5),
+    (None, 5),
+    (6, 5)
+])
+def test_get_all_insurances_with_valid_insurances_limit_partitions(
+        mySQLInsuranceRepository, valid_insurances_limit, expected_amount_of_insurances
+):
+    insurances = insurances_service.get_all(
+        repository=mySQLInsuranceRepository,
+        insurances_limit=valid_insurances_limit
+    )
+
+    assert isinstance(insurances, list), \
+        f"Expected instance of list, but got: {type(insurances).__name__}"
+
+    actual_amount_of_insurances = len(insurances)
+
+    assert actual_amount_of_insurances == expected_amount_of_insurances, \
+        f"Expected {expected_amount_of_insurances} insurances, but got: {actual_amount_of_insurances}"
+
+    for insurance in insurances:
+        assert isinstance(insurance, InsuranceReturnResource), \
+            f"Expected instance of InsuranceReturnResource, but got: {type(insurance).__name__}"
+
+
 # INVALID TESTS FOR get_all_insurances
+
+@pytest.mark.parametrize("invalid_insurances_limit, expected_error_message", [
+    ("1", "insurances_limit must be of type int or None, not str."),
+    (1.0, "insurances_limit must be of type int or None, not float."),
+    (True, "insurances_limit must be of type int or None, not bool."),
+    (False, "insurances_limit must be of type int or None, not bool."),
+])
+def test_get_all_insurances_with_invalid_insurances_limit_partitions(
+        mySQLInsuranceRepository, invalid_insurances_limit, expected_error_message
+):
+    with pytest.raises(TypeError, match=expected_error_message):
+        insurances_service.get_all(
+            repository=mySQLInsuranceRepository,
+            insurances_limit=invalid_insurances_limit
+        )
+
+
+@pytest.mark.parametrize("invalid_repository, expected_error_message", [
+    (None, "repository must be of type InsuranceRepository, not NoneType."),
+    ("repository", "repository must be of type InsuranceRepository, not str."),
+    (True, "repository must be of type InsuranceRepository, not bool."),
+    (123, "repository must be of type InsuranceRepository, not int."),
+])
+def test_get_all_insurance_with_invalid_repository_partitions(
+        invalid_repository, expected_error_message
+):
+    with pytest.raises(TypeError, match=expected_error_message):
+        insurances_service.get_all(repository=invalid_repository)
+
+
+def test_get_all_insurances_with_invalid_repository_partition_types(
+        mySQLAccessoryRepository, mySQLCustomerRepository, mySQLBrandRepository
+):
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLAccessoryRepository).__name__}."):
+        insurances_service.get_all(repository=mySQLAccessoryRepository)
+
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLCustomerRepository).__name__}."):
+        insurances_service.get_all(repository=mySQLCustomerRepository)
+
+    with pytest.raises(TypeError,
+                       match=f"repository must be of type InsuranceRepository, "
+                             f"not {type(mySQLBrandRepository).__name__}."):
+        insurances_service.get_all(repository=mySQLBrandRepository)
